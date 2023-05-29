6088074f2688e5989358525995b0dbbe0004afb6
unknown <gusgkr0117@naver.com>
2023-05-28 17:41:55 -0700

    Add MultiplicativeIdentity trait to FiniteFieldElement

--- src/fp.rs
+++ src/fp.rs
@@ -10,7 +10,7 @@ use impl_ops::*;
 use std::ops;
 
 /// Base trait for Field types
-pub trait Field<'a, T:FieldElement<'a>> : DynZero<'a, Output=T> + Clone {}
+pub trait Field<'a, T:FieldElement<'a>> : DynZero<'a, Output=T> + DynOne<'a, Output=T> + Clone {}
 /// FieldElement must refer a Field
 /// Thus it must take as input the lifetime of the Field
 pub trait FieldElement<'a> : Add<Output=Self> + Sub<Output=Self> + Mul<Output=Self> + Neg<Output=Self> + 
@@ -21,6 +21,13 @@ MultiplicativeInverse + Sized + PartialEq + Clone + fmt::Debug {}
 pub trait DynZero<'a> {
     type Output : FieldElement<'a>;
     fn zero(self) -> Self::Output;
+}
+
+/// Trait for the multiplicative identity of a dynamic field type
+/// Output a field element
+pub trait DynOne<'a> {
+    type Output : FieldElement<'a>;
+    fn one(self) -> Self::Output;
 }
 
 /// Field elements has its multiplicative inverse
@@ -64,6 +71,14 @@ impl<'a> DynZero<'a> for &'a FiniteField{
     type Output = FiniteFieldElement<'a>;
     fn zero(self) -> FiniteFieldElement<'a> {
         FiniteFieldElement::new(self, &BigInt::zero())
+    }
+}
+
+/// Implement DynOne for [FiniteField](FiniteField)
+impl<'a> DynOne<'a> for &'a FiniteField{
+    type Output = FiniteFieldElement<'a>;
+    fn one(self) -> FiniteFieldElement<'a> {
+        FiniteFieldElement::new(self, &BigInt::one())
     }
 }
 
